ce11b42a1662285a79b14f52627acff6a65bed90
violetferret <kahlstro@ucsc.edu>
2024-04-14 21:43:06 -0700

    writing myStory file, engine mostly done

--- rules.js
+++ rules.js
@@ -1,24 +1,27 @@
 class Start extends Scene {
     create() {
-        this.engine.setTitle("Title goes here"); // TODO: replace this text using this.engine.storyData to find the story title
+        this.engine.setTitle(this.engine.storyData.Title); // TODO: replace this text using this.engine.storyData to find the story title
         this.engine.addChoice("Begin the story");
     }
 
     handleChoice() {
-        this.engine.gotoScene(Location, "Home"); // TODO: replace this text by the initial location of the story
+        this.engine.gotoScene(Location, this.engine.storyData.InitialLocation); // TODO: replace this text by the initial location of the story
     }
 }
 
 class Location extends Scene {
     create(key) {
-        let locationData = undefined; // TODO: use `key` to get the data object for the current story location
-        this.engine.show("Body text goes here"); // TODO: replace this text by the Body of the location data
-        
-        if(true) { // TODO: check if the location has any Choices
-            for(let choice of ["example data"]) { // TODO: loop over the location's Choices
-                this.engine.addChoice("action text"); // TODO: use the Text of the choice
-                // TODO: add a useful second argument to addChoice so that the current code of handleChoice below works
-            }
+        let locationData = this.engine.storyData.Locations[key]; // TODO: use `key` to get the data object for the current story location
+        this.engine.show(locationData.Body); // TODO: replace this text by the Body of the location data
+        if(locationData.Choices) { // TODO: check if the location has any Choices
+            for(let choice of [locationData.Choices]) { // TODO: loop over the location's Choices
+                // IDK ABOUT THIS
+                let next = this.engine.storyData.Locations[choice[0].Target]
+                for (let i = 0; i < locationData.Choices.length; i++) {
+                    this.engine.addChoice(choice[i].Text, choice[i]); // TODO: use the Text of the choice
+                }
+                // TODO: add a useful second argument to addChoice so that the current code of handleChoice below works                
+            }     
         } else {
             this.engine.addChoice("The end.")
         }
